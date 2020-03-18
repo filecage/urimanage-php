9ca2b83ee29bbc87af53895a035c813d49cff222
filecage <mail@dbeuchert.com>
2020-03-18 17:06:06 +0100

    isNull has been removed from QueryParameter

--- tests/QueryParameterComponentTest.php
+++ tests/QueryParameterComponentTest.php
@@ -10,21 +10,14 @@
 
         /**
          * @param mixed $value
-         * @param bool $expectedIsNull
          * @param bool $expectedBool
          * @param string $expectedString
-         * @param string $expectedInt
+         * @param int $expectedInt
          *
          * @dataProvider provideTypeCastableParameters
          */
-        function testExpectsCorrectParameterCasting ($value, bool $expectedIsNull, bool $expectedBool, string $expectedString, int $expectedInt) {
+        function testExpectsCorrectParameterCasting ($value, bool $expectedBool, string $expectedString, int $expectedInt) {
             $queryParameter = new QueryParameter('test', $value);
-
-            if ($expectedIsNull === true) {
-                $this->assertTrue($queryParameter->isNull(), 'Expected QueryParameter to be null');
-            } else {
-                $this->assertFalse($queryParameter->isNull(), 'Expected QueryParameter to not be null');
-            }
 
             if ($expectedBool === true) {
                 $this->assertTrue($queryParameter->getValueAsBool(), 'Expected QueryParameter as boolean to be true');
@@ -41,14 +34,14 @@
          * @return Generator
          */
         function provideTypeCastableParameters () : Generator {
-            yield '(string) foo' => ['foo', false, true, 'foo', 0];
-            yield '(string) true' => ['true', false, true, 'true', 0];
-            yield '(string) false' => ['false', false, true, 'false', 0];
-            yield '(string) 1' => ['1', false, true, '1', 1];
-            yield '(string) 0' => ['0', false, false, '0', 0];
-            yield 'null' => [null, true, false, '', 0];
-            yield '(bool) true' => [true, false, true, '1', 1];
-            yield '(bool) false' => [false, false, false, '', 0];
+            yield '(string) foo' => ['foo', true, 'foo', 0];
+            yield '(string) true' => ['true', true, 'true', 0];
+            yield '(string) false' => ['false', true, 'false', 0];
+            yield '(string) 1' => ['1', true, '1', 1];
+            yield '(string) 0' => ['0', false, '0', 0];
+            yield 'null' => [null, false, '', 0];
+            yield '(bool) true' => [true, true, '1', 1];
+            yield '(bool) false' => [false, false, '', 0];
         }
 
     }
